a6a7ef1b4f899f92972a39fb7174ec700732949f
feliperod0519 <feliperod0519@hotmail.com>
2022-09-04 00:55:02 -0400

    2022-09-04 - S2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        /*
+        private readonly ILogger<UsersController> _logger;
+
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+        */
+        public DataContext _context { get; }
+
+        public UsersController(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+/*
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }*/
+    }
+}
