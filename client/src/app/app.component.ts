07bae74c5851924f016f8388fc333d65d9fdac66
feliperod0519 <feliperod0519@hotmail.com>
2022-09-06 01:18:20 -0400

    2022-09-06-5-1

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +12,13 @@ export class AppComponent implements OnInit {
   title = 'Testing App!';
   users: any;
 
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient, private accountService: AccountService){
   
   }
 
   ngOnInit(){
     this.getUsers();
+    this.setCurrentUser();
   }
 
   getUsers(){
@@ -27,6 +30,11 @@ export class AppComponent implements OnInit {
                    console.log(e);
                  }
     });
+  }
+
+  setCurrentUser(){
+    const user:User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
 }
 
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+
 
 @Component({
   selector: 'app-nav',
@@ -9,24 +12,40 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
 
   model:any = {}
-  loggedIn:boolean;
+  //loggedIn:boolean;
+  //currentUser$: Observable<User>;
 
-  constructor(private accountService:AccountService) { }
+  //constructor(private accountService:AccountService) { }
+  constructor(public accountService:AccountService) { }
 
   ngOnInit(): void {
+    //this.getCurrentUser();
+    //this.currentUser$ = this.accountService.currentUser$;
   }
 
   login(){
     //console.log(this.model);
     this.accountService.login(this.model).subscribe({ next: (r)=>{
                                                                     console.log(r);
-                                                                    this.loggedIn = true;
+                                                                    //this.loggedIn = true;
                                                                   },
                                                       error:(e)=>{ console.log(e); }
                                                     });
   }
 
   logout(){
-    this.loggedIn = false;
+    //this.loggedIn = false;
+    this.accountService.logout();
   }
+
+  /*
+  getCurrentUser(){
+    this.accountService.currentUser$.subscribe({
+      next:(user)=>{
+        this.loggedIn = !!user;
+       },
+      error:(e)=>{console.log(e);}
+    })
+  }
+  */
 }
