c0deda687a4d3c2b90d0a4fecf17b748096522ae
feliperod0519 <feliperod0519@hotmail.com>
2022-09-10 01:00:21 -0400

    2022-09-10-9

--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -2,3 +2,29 @@ export interface User{
     username: string;
     token:string;
 }
+
+/*
+let data: number | string =43;
+data=10;
+data="hello";
+
+interface Car{
+    color: string;
+    model: string;
+    topSpeed?: number
+}
+
+let car1:Car={
+    color:"blue",
+    model:'fiat'
+}
+
+
+let car2:Car={
+    color:"blue",
+    model:'fiat',
+    topSpeed:100
+}
+
+const multiply=(x:number,y:number):number=>{return x*y;};
+*/
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MembersService } from 'src/app/_services/members.service';
+import { Member } from 'src/app/_models/member';
 
 @Component({
   selector: 'app-member-list',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
+  members:Member[];
 
-  constructor() { }
+  constructor(private membersService: MembersService) { }
 
   ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers(){
+    this.membersService.getMembers().subscribe({
+      next:(members:Member[])=>{
+        this.members = members;
+      },
+      error:(e)=>{console.log(e);}
+    });
   }
 
 }
